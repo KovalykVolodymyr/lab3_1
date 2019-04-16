e0fd0539a4d35a012372ccdff039bd428b26d6ff
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-04-16 16:26:55 +0200

    invoiceBeEqualToItemsTotalCostSumm and update old tests

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -28,10 +28,10 @@ import static org.mockito.Mockito.*;
 public class BookKeeperTest {
     private InvoiceRequest invoiceRequest;
     private BookKeeper keeper;
+    private Money money;
 
     @Mock
     private TaxPolicy taxPolicy;
-
 
 
     @Before
@@ -39,32 +39,33 @@ public class BookKeeperTest {
         ClientData client = new ClientData(Id.generate(), "Filip");
         keeper = new BookKeeper(new InvoiceFactory());
         invoiceRequest = new InvoiceRequest(client);
-        when(taxPolicy.calculateTax(ProductType.FOOD, Money.ZERO)).thenReturn(new Tax(Money.ZERO, "food"));
-        when(taxPolicy.calculateTax(ProductType.STANDARD, Money.ZERO)).thenReturn(new Tax(Money.ZERO, "standard"));
+        money = new Money(10);
+        when(taxPolicy.calculateTax(ProductType.FOOD, money)).thenReturn(new Tax(new Money(10), "food"));
+        when(taxPolicy.calculateTax(ProductType.STANDARD,money)).thenReturn(new Tax(new Money(10), "standard"));
     }
 
     @Test
-    public void  givenInvoiceRequestWithSingleItem(){
+    public void givenInvoiceRequestWithSingleItem() {
         ProductData productData = new ProductData(Id.generate(), Money.ZERO, "Product", ProductType.FOOD, new Date());
-        RequestItem item = new RequestItem(productData,1,Money.ZERO);
+        RequestItem item = new RequestItem(productData, 1, this.money);
         invoiceRequest.add(item);
         Invoice invoice = keeper.issuance(invoiceRequest, taxPolicy);
-        Assert.assertThat(invoice.getItems().size(),is(1));
+        Assert.assertThat(invoice.getItems().size(), is(1));
 
 
     }
 
     @Test
-    public void  givenInvoiceRequestWithTwoItem(){
+    public void givenInvoiceRequestWithTwoItem() {
         ProductData productData = new ProductData(Id.generate(), Money.ZERO, "Product", ProductType.FOOD, new Date());
         ProductData productData1 = new ProductData(Id.generate(), Money.ZERO, "Product", ProductType.STANDARD, new Date());
-        RequestItem item = new RequestItem(productData,1,Money.ZERO);
-        RequestItem item1 = new RequestItem(productData1,1,Money.ZERO);
+        RequestItem item = new RequestItem(productData, 1, this.money);
+        RequestItem item1 = new RequestItem(productData1, 1, this.money);
         invoiceRequest.add(item);
         invoiceRequest.add(item1);
         keeper.issuance(invoiceRequest, taxPolicy);
-        verify(taxPolicy, times(1)).calculateTax(ProductType.FOOD, Money.ZERO);
-        verify(taxPolicy, times(1)).calculateTax(ProductType.STANDARD, Money.ZERO);
+        verify(taxPolicy, times(1)).calculateTax(ProductType.FOOD, this.money);
+        verify(taxPolicy, times(1)).calculateTax(ProductType.STANDARD,this.money);
 
     }
 
@@ -72,7 +73,7 @@ public class BookKeeperTest {
     public void invoiceRequestWithoutItemsBeEmpty() {
         Invoice invoice = keeper.issuance(invoiceRequest, taxPolicy);
         invoice.getGros();
-        Assert.assertThat(invoice.getItems().size(),is(0));
+        Assert.assertThat(invoice.getItems().size(), is(0));
     }
 
     @Test
@@ -81,8 +82,18 @@ public class BookKeeperTest {
         verifyZeroInteractions(taxPolicy);
     }
 
+    @Test
+    public void invoiceBeEqualToItemsTotalCostSumm() {
+        ProductData productData = new ProductData(Id.generate(), Money.ZERO, "Product", ProductType.FOOD, new Date());
+        ProductData productData1 = new ProductData(Id.generate(), Money.ZERO, "Product", ProductType.STANDARD, new Date());
+        RequestItem item = new RequestItem(productData, 1,this.money);
+        RequestItem item1 = new RequestItem(productData1, 1, this.money);
+        invoiceRequest.add(item);
+        invoiceRequest.add(item1);
 
-
+        Invoice invoice = keeper.issuance(invoiceRequest, taxPolicy);
+        Assert.assertThat(invoice.getNet(), is(money.add(money)));
+    }
 
 
 }
